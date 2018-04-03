7df12f05bf4ac66cb96f8f77a41e52ed54a3946d
Kim Kosman <kkosman1@nd.edu>
2018-04-02 21:18:06 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/login');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Track My Path' });
+});
+
+router.get('/home', function(req, res, next){
+  res.render('home');
+});
+
+module.exports = router;
